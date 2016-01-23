0b70236b80838b04f547a9383e42762985a04ab1
Sylvain Bekaert <sylvain.bekaert@gmail.com>
2016-01-23 12:05:42 +0100

    Tmp: add _id to contacts

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -3,6 +3,9 @@ const Contact = require('../models/contact.js');
 
 module.exports = function(router) {
   router.get('/contacts', (req, res) => {
-    res.json(addressBook);
+    res.json(addressBook.map((address, i) => {
+      address._id = i;
+      return address;
+    }));
   });
 };
