d31d4868c8f11ccac917dc18c3732da1bb40ec24
Sylvain Bekaert <sylvain.bekaert@gmail.com>
2016-01-23 11:48:17 +0100

    Tmp: add routes to get contacts with mocked data

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const app = express();
 mongoose.connect('mongodb://localhost/contacts');
 
+const addressBook = require('./data/addressBook.json');
+
 const db = mongoose.connection;
 db.on('error', (e) => logger.error('connection error:', e));
 db.once('open', () => {
@@ -14,11 +16,8 @@ db.once('open', () => {
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/info', (req, res) => {
-  res.json({
-    viewSystem: 'React',
-    architecture: 'Redux',
-  });
+app.get('/contacts', (req, res) => {
+  res.json(addressBook);
 });
 
 app.listen(3000, () => logger.info('App running on port 3000 o//'));
