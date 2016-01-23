219a29ed29f83e233e8c2991b43d88dc29b9c7f0
florentduveau <florent.duveau@gmail.com>
2016-01-23 11:43:51 +0100

    [MOCK] Add front data

---
+++ public/mock/data.js
@@ -0,0 +1,138 @@
+export default [
+  {
+    "number": "1",
+    "street": "Rue Saint-Laurent",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Aaron",
+    "lastname": "Desamparo"
+  },
+  {
+    "number": "123",
+    "street": "Rue Du Faubourg Saint-Martin",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Abbey",
+    "lastname": "Desan"
+  },
+  {
+    "number": "72",
+    "street": "Rue De Dantzig",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Abbie",
+    "lastname": "Aaby"
+  },
+  {
+    "number": "123",
+    "street": "Rue Du Faubourg Saint-Martin",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Abbie",
+    "lastname": "Desanctis"
+  },
+  {
+    "number": "121",
+    "street": "Rue Du Faubourg Saint-Martin",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Abby",
+    "lastname": "Desando"
+  },
+  {
+    "number": "127",
+    "street": "Rue FalguiÃ¨re",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Abe",
+    "lastname": "Aakre"
+  },
+  {
+    "number": "251",
+    "street": "Rue Marcadet",
+    "city": "Paris 18e Arrondissement",
+    "postcode": "75018",
+    "firstname": "Abel",
+    "lastname": "Aaland"
+  },
+  {
+    "number": "146",
+    "street": "Rue Du Faubourg Saint-Martin",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Abdul",
+    "lastname": "Desano"
+  },
+  {
+    "number": "127",
+    "street": "Rue FalguiÃ¨re",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adelina",
+    "lastname": "Aas"
+  },
+  {
+    "number": "127",
+    "street": "Rue FalguiÃ¨re",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adeline",
+    "lastname": "Aasby"
+  },
+  {
+    "number": "127",
+    "street": "Rue FalguiÃ¨re",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adell",
+    "lastname": "Aase"
+  },
+  {
+    "number": "3",
+    "street": "Rue Yvart",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adella",
+    "lastname": "Aasen"
+  },
+  {
+    "number": "18",
+    "street": "Rue Saint-Laurent",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Adam",
+    "lastname": "Desautelle"
+  },
+  {
+    "number": "221",
+    "street": "Rue De Lane Convention",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adena",
+    "lastname": "Abad"
+  },
+  {
+    "number": "221",
+    "street": "Rue De Lane Convention",
+    "city": "Paris 15e Arrondissement",
+    "postcode": "75015",
+    "firstname": "Adina",
+    "lastname": "Abadi"
+  },
+  {
+    "number": "20",
+    "street": "Rue Saint-Laurent",
+    "city": "Paris 10e Arrondissement",
+    "postcode": "75010",
+    "firstname": "Adam",
+    "lastname": "Desautel"
+  },
+  {
+    "number": "58",
+    "street": "Rue Des Plantes",
+    "city": "Paris 14e Arrondissement",
+    "postcode": "75014",
+    "firstname": "Adolph",
+    "lastname": "Abair"
+  }
+]
