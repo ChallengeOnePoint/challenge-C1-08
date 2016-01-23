d4315f3caee5ff8f5dc876be358acca95003d3e2
augustin roux <augustin.roux@gmail.com>
2016-01-23 13:58:30 +0100

    [UI] Beta for Search

--- public/scripts/components/Search.jsx
+++ public/scripts/components/Search.jsx
@@ -1,9 +1,39 @@
 /* eslint no-unused-vars: 0 */
 import React, {PropTypes} from 'react';
 /* eslint no-unused-vars: 1 */
+import {connect} from 'react-redux';
+import ContactList from './ContactList.jsx';
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({contacts}) => {
+
+  let result = [];
+
+  function search(e) {
+    const queryString = e.target.value;
+    debugger;
+    contacts.map((contact) => {
+      const {firstName, lastname} = contact;
+      debugger;
+      if (firstName.includes(queryString) || lastName.includes(queryString)) {
+        result.push(contact);
+      }
+    })
+  }
+
+  return (
+    <div className="search">
+      <input type="text" placeholder="Chercher un contact" onKeyPress={search}></input>
+      <button onClick={search}><img src="./loupe.svg"/></button>
+      <div className="contactList">{result}</div>
+      <ContactList contacts={result}/>
+    </div>
+  );
 }
 
-export default Search;
+function select(state) {
+  return {
+    contacts: state.contacts,
+  }
+}
+
+export default connect(select)(Search);
